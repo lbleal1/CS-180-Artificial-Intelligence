04c9a8a066404be5a2e7c89f245fb21dc4839fd0
rpscruz <rscruz7@up.edu,ph>
2019-03-11 16:24:32 +0800

    plotting line between 2 given points works

--- mp.py
+++ mp.py
@@ -1,42 +1,32 @@
-from numpy import zeros, ones, vstack
+from numpy import zeros, ones, vstack, arange
 from numpy.linalg import lstsq
 
-
-DIM_Y = 10
+DIM_Y = 20
 DIM_X = 20
 
-
 def getx(x):
-    return int(DIM_Y / 2) + x - 1
+    return int(DIM_X / 2) + x
 
 def gety(y):
-    return int(DIM_X / 2) + y - 1
+    return -(int(DIM_Y / 2) + y + 1) % DIM_Y 
 
 grid = zeros((DIM_Y, DIM_X))
 print(grid)
 # https://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
 
-points = [(0,0),(7,6)]
+points = [(0,0),(2,7)]
 x_coords, y_coords = zip(*points)
 A = vstack([x_coords,ones(len(x_coords))]).T
 m, c = lstsq(A, y_coords)[0]
 
-for x in range(1, 5):
-    grid[gety(int(round(m*x+c)))][getx(x)] = 1
-    print(getx(x), gety(int(round(m*x + c) ) ) )
+for x in arange(0, 2, 0.1):
+    grid[gety(int(round(m*x + c)))][getx(int(x))] = 1
+    print(int(x), int(round(m*x + c) ))
+    #print(getx(x), gety(int(round(m*x + c) ) ) )
+    #print(1)
 
 print(grid)
-'''
 
-
-
-print(getM(-3, 3, 3, -1))
-
-print(gety(1))
-
-#print(A)
-
-'''
 
 '''
 for x in grid[x]:
